0f541152ab666ab284ebdeae60daac369df655fa
Vinicius-O-Ferraz <vinicius.f.ferraz.bsi@gmail.com>
2024-04-27 08:41:05 -0300

    feat: implementing activity 3 code

--- criptograpyCode/activity3.py
+++ criptograpyCode/activity3.py
@@ -1,6 +1,9 @@
 #Atividade 3
 from random import randint
+from collections import Counter
+import math
 from activity1 import Alphabet
+reload(activity1)
 
 stringMessage =                     """
 De tudo, ao meu amor serei atento
@@ -24,4 +27,35 @@ Mas que seja infinito enquanto dure.
 
 key = randint(0,26)
 alphabet = Alphabet.charRange('a','z')
-print(alphabet)
+LETTER_FREQUENCY_PTBR = {
+                    'a': 14.63, 'b': 1.04, 'c': 3.88, 'd': 4.99,'e':12.57,'f':1.02,'g':1.30,'h':1.28,'i':6.18,'j':0.40,
+                    'k':0.02,'l':2.78,'m':4.74,'n':5.05,'o':10.73,'p':2.52,'q':1.20,'r':6.53,'s':7.81,'t':4.34,'u':4.63,
+                    'v':1.67,'w':0.01,'x':0.21,'y':0.01,'z':0.47
+                    }
+
+def caesaerCypher(message, key):
+     return Alphabet.offset(message,key)
+
+def difference(message):
+    counter = Counter(message)
+    return sum([abs(counter.get(letter, 0) * 100 / len(message) - LETTER_FREQUENCY_PTBR[letter]) for letter in
+                alphabet]) / len(alphabet)
+
+def breakCipher(cipherText):
+    lowest_difference = math.inf
+    encryption_key = 0
+
+    for key in range(1, len(alphabet)):
+        current_plain_text = caesaerCypher(cipherText, - key)
+        current_difference = difference(current_plain_text)
+
+        if current_difference < lowest_difference:
+            lowest_difference = current_difference
+            encryption_key = key
+
+    return encryption_key
+
+cipher = Alphabet.clearText(stringMessage)
+decript = caesaerCypher(cipher, -key)
+print(key)
+print(breakCipher(cipher))
